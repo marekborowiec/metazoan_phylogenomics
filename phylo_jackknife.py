92acc91f99574daa09a8ecd896dcf5a2fdb95e52
marekborowiec <petiolus@gmail.com>
2015-04-27 20:43:38 -0400

    fixed bugs, added handling of amino acid sequences and removal of directories after concatenation

--- phylo_jackknife.py
+++ phylo_jackknife.py
@@ -30,6 +30,11 @@ It should contain only single-locus alignments in FASTA or NEXUS format.
 example: /home/user/Data/AlignedFiles""")
 dirPath = raw_input(prompt)
 
+print("""What is the path to your phyutility.jar file?
+example: /home/user/Phylo-Software/Phyutility/phyutility.jar""")
+phyutDir = "java -jar " + raw_input(prompt) + " -concat -in "
+#print phyutDir
+
 print("""How many loci do you want to use for your replicate?
 example: 20""")
 lociNo = raw_input(prompt)
@@ -37,11 +42,6 @@ lociNo = raw_input(prompt)
 print("""How many jackknife replicates do you want to perform?
 example: 200""")
 replicatesNo = raw_input(prompt)
-
-print("""What is the path to your phyutility.jar file?
-example: /home/user/Phylo-Software/Phyutility/phyutility.jar""")
-phyutDir = "java -jar " + raw_input(prompt) + " -concat -in "
-#print phyutDir
 
 print("""Are your sequences amino acid or DNA?
 (aa/dna)""")
@@ -51,25 +51,36 @@ seqType = raw_input(prompt)
 filenames = [f for f in os.listdir(dirPath) if os.path.isfile(dirPath + '/' + f)]
 #print(filenames)
 #print(len(filenames))
+
+directories = []
 for  i in range(1, int(replicatesNo) + 1):
 
 	randomFiles = random.sample(filenames, int(lociNo))
 	#print(randomFiles)
-	destDirectory = dirPath + '/Sample_' + lociNo + '_' + str(i) + '/'
+	destDirectory = dirPath + '/Sample_' + lociNo + '_' + str(i) + "/"
 	print(destDirectory)
 	os.mkdir(destDirectory)
-
+	directories.append(destDirectory)
 	for fname in randomFiles:
 
-    		srcPath = os.path.join(dirPath, fname)
+		srcPath = os.path.join(dirPath, fname)
 		destFile = destDirectory + fname
-    		shutil.copyfile(srcPath, destFile)
+		shutil.copyfile(srcPath, destFile)
 
-directories = [x[0] for x in os.walk(dirPath)]
-print directories
+counter = 1
+for directory in directories:
+	#print(directory)
+	call_string = phyutDir + directory + '* ' + \
+	 '-out ' + dirPath + '/Sample_' + lociNo + '_' + str(counter) + '.nex'
+	print(call_string)
+	counter += 1
+	subprocess.call(call_string, shell=True)
+
+if seqType == 'aa':
+	call_string = "sed -i 's/DATATYPE = DNA/DATATYPE = AA/g' Sample*nex"
+	print(call_string)
+	subprocess.call(call_string, shell=True)
 
 for directory in directories:
-        #print directory
-        call_string = phyutDir + directory + '/* ' + '-out ' + directory + '.nex'
-        print call_string
-        subprocess.call(call_string, shell=True)
+	print("Removing directory " + directory) + " and its contents..."
+	shutil.rmtree(directory)
